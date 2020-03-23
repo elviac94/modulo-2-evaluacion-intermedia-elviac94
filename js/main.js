c60af84f50fae9e0387f470971bd15a80ffdd850
Elvira <elvi_ac94@hotmail.com>
2020-03-23 16:30:44 +0100

    Fix bugs

--- js/main.js
+++ js/main.js
@@ -1,52 +1,45 @@
 'use strict'
-const inputNumber= document.querySelector('.number-selected');
-const submitButton= document.querySelector('.submit-button');
-const track= document.querySelector('.track');
-const numberAttempsTotal= document.querySelector('.number-attemps');
+const inputNumber = document.querySelector('.number-selected');
+const submitButton = document.querySelector('.submit-button');
+const track = document.querySelector('.track');
+const numberAttempsTotal = document.querySelector('.number-attemps');
 
-let max= 100;
-let  numberAttemps= 0;
+let maxNumber = 100;
+let numberAttemps = 0;
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-   }
-   console.log(getRandomNumber(max));
+}
+const numberRandom = getRandomNumber(maxNumber);
+console.log(numberRandom)
 
-   const numberRandom = getRandomNumber(max);
-   
-   function showtrack(){
-       
-       if(inputNumber.value < numberRandom) {
-          
-          return track.innerHTML= ' Pista: Demasiado bajo';
-       }
-        else if(inputNumber.value > numberRandom ){
-            return track.innerHTML= ' Pista: Demasiado alto';
-       }
-        else if(inputNumber.value == numberRandom ){
-           return track.innerHTML= 'Pista: Has ganado campeona!!!';
-       }
-       else{
-           return track.innerHTML= 'Pista: El número debe estar entre 1 y 100';
-       }
-          
-   }
-   
+function showtrack() {
 
-   function onClickButton(){
-       showtrack();
-       attemps();
-       numberAttemps++
-       
+    if (inputNumber.value <= 0 || inputNumber.value > maxNumber || inputNumber.value === '') {
 
+         track.innerHTML = 'Pista: El número debe estar entre 1 y 100';
+    }
+    else if (inputNumber.value < numberRandom) {
+         track.innerHTML = ' Pista: Demasiado bajo';
+    }
+    else if (inputNumber.value > numberRandom) {
+         track.innerHTML = ' Pista: Demasiado alto';
+    } else {
+         track.innerHTML = 'Pista: Has ganado campeona!!!';
+    }
+}
+
+function onClickButton() {
+    numberAttemps++
+    attemps();
+    showtrack();
     console.log(`Número escogido: ${inputNumber.value}`);
-
 }
 
-function attemps(){
-  return numberAttempsTotal.innerHTML= ` nº de intentos: ${numberAttemps}`;
-
+function attemps() {
+    return numberAttempsTotal.innerHTML = ` nº de intentos: ${numberAttemps}`;
 }
 
+submitButton.addEventListener('click', onClickButton);
 
-submitButton.addEventListener('click',onClickButton);
+
